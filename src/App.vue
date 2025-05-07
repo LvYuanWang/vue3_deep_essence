465d9d3bcef00a94662418282e457a704e2c9340
LvYuanWang <1175430934@qq.com>
2025-05-07 20:42:10 +0800

    vue3虚拟DOM的本质

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app-container">
+    <h1>App</h1>
+    <Child name="李四" email="123@qq.com" />
+    <component :is="vNode" />
+  </div>
+</template>
+
+<script setup>
+import { h } from 'vue'
+import Child from './components/Child.vue'
+
+const vNode = h(Child, {
+  name: '张三',
+  email: '123@qq.com',
+})
+console.log('vNode: ', vNode) // 打印虚拟节点
+</script>
+
+<style scoped>
+.app-container {
+  width: 400px;
+  border: 1px solid;
+  padding: 10px;
+  box-sizing: border-box;
+}
+</style>
