07049d6beff6f66fff4739ba34117844bd438001
LvYuanWang <1175430934@qq.com>
2025-06-10 16:49:44 +0800

    vue3指令的本质

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,12 @@
 <template>
-  <div class="app-container">
-    <h1>App</h1>
-    <Child name="李四" email="123@qq.com" />
-    <component :is="vNode" />
-  </div>
+  <div>{{ count }}</div>
+  <button v-on:click="count++">count++</button>
 </template>
 
 <script setup>
-import { h } from 'vue'
-import Child from './components/Child.vue'
+import { ref } from 'vue'
 
-const vNode = h(Child, {
-  name: '张三',
-  email: '123@qq.com',
-})
-console.log('vNode: ', vNode) // 打印虚拟节点
+const count = ref(1)
 </script>
 
-<style scoped>
-.app-container {
-  width: 400px;
-  border: 1px solid;
-  padding: 10px;
-  box-sizing: border-box;
-}
-</style>
+<style scoped></style>
